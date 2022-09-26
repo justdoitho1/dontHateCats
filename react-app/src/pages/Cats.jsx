2a8178b04a11d62b4bb33152c533e8187c2e4d44
justdoitho1 <gyejeol01@gmail.com>
2022-09-27 00:27:29 +0900

    style : Btn 중복 코드 통합, BtnBox padding 적용, todo 추가

--- react-app/src/pages/Cats.jsx
+++ react-app/src/pages/Cats.jsx
@@ -3,8 +3,8 @@ import ajax_get from "../api/Api";
 import styled from "styled-components";
 
 /**
- * todo : 모달 버튼 CSS, 로직 추가, 길이에 따른 위치 조정(미정), FADE OUT
- * @returns 
+ * todo : 모달 버튼 CSS, 로직 추가, 길이에 따른 위치 조정(미정), FADE OUT, 고양이가 싫으세요? 질문 대답에 아니오 시 로직 추가 필요
+ * @returns
  */
 const Cats = () => {
   const [url, setUrl] = useState("");
@@ -77,108 +77,101 @@ const Cats = () => {
 
   return (
     <>
-    <Main>
-      <LogoBox>
-        <h1>Don't Hate Cats</h1>
-        <LogoImg></LogoImg>
-      </LogoBox>
-      <CatsImgBox>
-        {{ url } && <CatsImg src={url} alt="cats" width={300} />}
-      </CatsImgBox>
-      <BtnBox>
-        <LikeBtn onClick={likeClickHandler}>좋아요</LikeBtn>
-        <DislikeBtn onClick={disLikeClickHandler}>싫어요</DislikeBtn>
-      </BtnBox>
-      {modalVisible && (
-        <ModalBox>
-          {afterDislikeMessageObj[disLikeCount]}
-          <br></br>
-          <button onClick={modalOkBtnHandler}>네</button>
-          <button>아니오</button>
-        </ModalBox>
-      )}
-      {warningModalVisible && (
-        <ModalBox>
-          {warningMessageObj[warningCount]}
-          <button onClick={warningModalOkBtnHandler}>
-            흥! 그래도 고양이가 싫어
-          </button>
-        </ModalBox>
-      )}
+      <Main>
+        <LogoBox>
+          <h1>Don't Hate Cats</h1>
+          <LogoImg></LogoImg>
+        </LogoBox>
+        <CatsImgBox>
+          {{ url } && <CatsImg src={url} alt="cats" width={300} />}
+        </CatsImgBox>
+        <BtnBox>
+          <LikeBtn backgroundColor={"yellow"} onClick={likeClickHandler}>
+            좋아요
+          </LikeBtn>
+          <LikeBtn onClick={disLikeClickHandler}>싫어요</LikeBtn>
+        </BtnBox>
+        {modalVisible && (
+          <ModalBox>
+            {afterDislikeMessageObj[disLikeCount]}
+            <br></br>
+            <BtnBox>
+              <button onClick={modalOkBtnHandler}>네</button>
+              <button>아니오</button>
+            </BtnBox>
+          </ModalBox>
+        )}
+        {warningModalVisible && (
+          <ModalBox>
+            {warningMessageObj[warningCount]}
+            <BtnBox>
+              <button onClick={warningModalOkBtnHandler}>
+                흥! 그래도 고양이가 싫어
+              </button>
+            </BtnBox>
+          </ModalBox>
+        )}
       </Main>
     </>
   );
 };
 
 const Main = styled.div`
-  position : absolute;
-`
+  position: absolute;
+`;
 
 const ModalBox = styled.div`
-  text-align : center;
-  width : 60%;
-  height : 10%;
-  position : fixed;
-  background-color : white;
+  text-align: center;
+  width: 60%;
+  height: 10%;
+  position: fixed;
+  background-color: white;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   margin: auto;
-  padding : 10%;
-  border-radius : 10px;
-`
+  padding: 10%;
+  border-radius: 10px;
+  opacity : 80%;
+`;
 
 const LogoBox = styled.div`
-  height : 10vh;
-  width : 100%;
-  text-align : center;
-`
+  height: 10vh;
+  width: 100%;
+  text-align: center;
+`;
+//이미지 할거면 추가
+const LogoImg = styled.img``;
 
-const LogoImg = styled.img`
-  
-`
-// 7
 const CatsImgBox = styled.div`
   text-align: center;
-  height : 60vh; 
-  overflow : hidden;
-  background-color : skyblue;
+  height: 60vh;
+  overflow: hidden;
+  //background-color: skyblue;
 `;
 
 const CatsImg = styled.img`
   width: 100%;
-  height : auto;
+  height: auto;
 `;
 
 const BtnBox = styled.div`
   text-align: center;
+  padding : 5% 0;
 `;
 
 const LikeBtn = styled.button`
-position: relative;
-border: none;
-display: inline-block;
-padding: 15px 30px;
-border-radius: 10px;
-box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
-text-decoration: none;
-font-weight: 600;
-transition: 0.25s;
-margin: 20px;
-background-color : yellow;
-`;
-
-const DislikeBtn = styled.button`
-position: relative;
-border: none;
-display: inline-block;
-padding: 15px 30px;
-border-radius: 10px;
-box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
-text-decoration: none;
-font-weight: 600;
-transition: 0.25s;
-margin: 20px;
+  position: relative;
+  border: none;
+  display: inline-block;
+  padding: 15px 30px;
+  border-radius: 10px;
+  box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+  font-weight: 600;
+  transition: 0.25s;
+  margin: 20px;
+  background-color: ${(props) => props.backgroundColor};
 `;
 export default Cats;
